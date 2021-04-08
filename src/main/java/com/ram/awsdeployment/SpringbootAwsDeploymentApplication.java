efd0dc5102e468e04f00648205dc3e004ff46d06
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2021-04-08 18:23:16 +0800

    Initial Commit

---
+++ src/main/java/com/ram/awsdeployment/SpringbootAwsDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.ram.awsdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/awsdeployment/SpringbootAwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.awsdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
